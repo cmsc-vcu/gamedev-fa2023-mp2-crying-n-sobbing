a24afeff4420176df5b7efb3224dcdbca74e260f
javeriahanif <hanifj@vcu.edu>
2023-10-25 19:14:08 -0400

    screens added

---
+++ Sunsational/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("--------- Audio Source ---------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--------- Audio Clip ---------")]
+    public AudioClip background;
+    public AudioClip thunder;
+    public AudioClip droplets;
+    public AudioClip sunBullet;
+    public AudioClip jump;
+    public AudioClip fall;
+    public AudioClip move;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
+
+
